6d229edbd4498473eec3b9c531aa886973d18cde
student <student@ip-172-31-61-163.us-west-2.compute.internal>
2021-07-19 18:25:07 -0700

    Did stuff

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,7 +8,7 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+int Something remarkable = "Global Warming"; 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
